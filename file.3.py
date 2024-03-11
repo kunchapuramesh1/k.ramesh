2fe70a5fa88c64f2b65a5d8da50bd2e74e090864
kunchapuramesh1 <162982044+kunchapuramesh1@users.noreply.github.com>
2024-03-11 16:00:33 +0530

    Add files via upload

---
+++ file.3.py
@@ -0,0 +1,17 @@
+# ---> print(object,sep,end,file,flush)
+# print has 5 parameters
+
+# object --> the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a,b,c, sep='$')
+
+# end --> which is used add the string at the end of the value,
+# # it will not keep the command to stay in same line
+# Eg:
+# print(str(a),b,end='%%')
+# print(str(c))
+
