083a1110d0bc31fe68df604e55ae4acad9051a41
kunchapuramesh1 <162982044+kunchapuramesh1@users.noreply.github.com>
2024-03-11 16:11:44 +0530

    Add files via upload

---
+++ file. 1.py
@@ -0,0 +1,28 @@
+print("hello")
+
+print("hello world")
+,,,
+
+print("hello world")
+print("hello world")
+print("hello world")
+,,,
+
+---->commenting
+1.')'commenting is used for making documentation in a python file
+2.')'when the line is commented , that line will not be considered as a part of the code
+3.')'when the program is executed,commented line will not be in part of execution
+
+---->Note:
+single line commenting can be done using # at the begining of the line
+     Eg:
+         #print("hello 1")
+multiple line commenting can be done
+
+
+import this
+print(this)
+
+
+
+
