87b3d21616ed33b6b4e94023e77048b181f2cc4e
kunchapuramesh1 <162982044+kunchapuramesh1@users.noreply.github.com>
2024-03-21 15:58:31 +0530

    Add files via upload

--- Day 10.py
+++ Day 10.py
@@ -131,11 +131,246 @@ obj.add(2)
 obj.add(3,4)
 obj.add(1,2,3)
 '''
+'''
+#  ----------->Abstraction
+# The process of hiding the implimentation details is abstraction
+
+# ? Eg:1
+from abc import ABC,abstractmethod
+class shapes(ABC):
+    @abstractmethod
+    def sides(self):
+        print("All shapes have sides except circle")
+
+class triangle(shapes):
+    def triangle_sides(self):
+        print("3 sides")
+
+        def name(self):
+        print("Iam traingle")
+
+
+class square(shapes):
+    def square(self):
+        print("4 sides")
+
+    def sides (self):
+       pass
+
+tr = triangle()
+tr.triangle_sides()
+tr.name()
+'''
+
+
+
+#! Rules to define abstract class1
+
+# To write the capital letters
+#1.) Abstract class cannot be instantiated
+#2.) From abc import ABC, abstractmethod
+#3.) subclass the normal class with ABC
+#4.) Convert the normal method inside the abstract class to abstract method
+#5.) All the child classes have to be subclassed with abstract class
+#6.) The abstract method should be present in the child classes
+
+'''
+#Eg:2
+#super()
+from abc import ABC,abstractmethod
+class c1(ABC):
+    @abstractmethod
+    def m1(self):
+        print("This is abstract class")
+
+class c2(c1):
+    def m2(self):
+        super().m1()
+        print("Iam child")
+
+    def m1(self):
+        pass
+        
+class2 = c2()
+class2.m2()
+'''
+
+'''
+#Eg:3
+from abc import ABC,abstractmethod
+class password(ABC):
+    @abstractmethod
+    def pwd(self):
+        pswd = "Ram12345"
+        return pswd
+
+class login (password):
+    def validate(self,name,password):
+        if super().ped() ==password:
+            print("welcome",name,'!!')
+            print("Login successfull")
+        else:
+            print("please check the password")
+
+    def pwd(self):
+        pass
+
+login = login()
+name = input("enter the name:")
+pwd = input("enter the password:")
+login.validate(name,pwd)
+'''
+# ! Encapsulation
+
+'''
+#Eg:1
+class car:
+    __name = "BMW"# private variable
+    print(__name)
+
+c1 = car()
+print(c1.name)
+c1.name = "Audi" 
+print(c1.name) #error
+'''
+
+'''
+#* --->Eg:2
+# ? Accessing private date outside the class
+class c1:
+    __phone = '1234567890'
+
+    def display(self):
+        print(self.__phone)
+c = c1()
+c.display()
+'''
+'''
+# * ---> Eg:3
+# ? declare private method
+class class1:
+    def m1(self):
+        print("iam private method")
+
+c = class1()
+c.__m1()# error
+
+
+# Nested class
+class class1:
+    class class2:
+        name = "sai"
+
+        def display(self):
+            print(self.name)
+
+    obj = class1
+    obj.obj1.display()
+    
+'''
+'''
+# 1.) Write the code for the below tasks using function
+d1 = {"shirt":1000, "pant":1500, "Shoes":900, "handkey":30}
+for val in d1:
+    if d1[val] == min(d1.values()):
+        print(val)
+for val in d1:
+    if d1[val] == max(d1.values()):
+        print(val)
+for val in d1:
+    if val.startswith('s') or val.startswith('S'):
+        print(val)
+'''
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+def decor(func):
+    def inner():
+        str1 = func()
+        return str1.upper()
+    return inner()
+
+@decor
+def greet():
+    return 'good morning'
+print(greet)
+'''
 
 
 
